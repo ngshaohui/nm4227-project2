986ac3e756c90f0a2b99a55a522be1396ec1c1b1
Shao Hui <shinlos@hotmail.com>
2020-03-20 02:04:34 +0800

    add motivational messages

--- utils.js
+++ utils.js
@@ -3,10 +3,22 @@ const SCREEN_WIDTH = 26
 const TILE_SIZE = 32
 const TIMEOUT_DURATION = 2000
 
+const POSITIVE_MESSAGES = [
+  'Come on you can do it',
+  'If Alex can do it, so can you!',
+]
+
 const TAUNTS = [
   'Can Alex do this in front of a class of 40 students?',
   'Alex is that you?',
   '"Rules in computer games are hidden and you can just \njump straight into it unlike a board game" - Alex',
 ]
 
-export { SCREEN_HEIGHT, SCREEN_WIDTH, TAUNTS, TILE_SIZE, TIMEOUT_DURATION }
+export {
+  POSITIVE_MESSAGES,
+  SCREEN_HEIGHT,
+  SCREEN_WIDTH,
+  TAUNTS,
+  TILE_SIZE,
+  TIMEOUT_DURATION,
+}
